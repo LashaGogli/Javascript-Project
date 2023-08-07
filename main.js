e00695f13f2ecbb0ab9d7b591c43b3c96c6fadd3
Beren Varol <beren.vrl@gmail.com>
2023-08-07 20:31:03 +0300

    use strict statement is added, some fun textx and emojies are added with explanatory sentences, also comments that explains functions are added

--- main.js
+++ main.js
@@ -1,70 +1,85 @@
+"use strict";
+
+//Function for computer play(random selection)
 function computerPlay() {
-    const choices = ["rock", "paper", "scissors"];
-    return choices[Math.floor(Math.random() * choices.length)];
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
+//Getting user input
 function getPlayerChoice() {
-    let playerChoice;
-    do {
-        playerChoice = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
-        if (!["rock", "paper", "scissors"].includes(playerChoice)) {
-            alert("Please enter either rock, paper, or scissors.");
-        }
-    } while (!["rock", "paper", "scissors"].includes(playerChoice));
-    return playerChoice;
+  let playerChoice;
+
+  //making input insensitive for upper/lowercases
+  do {
+    playerChoice = prompt(
+      "Hey! This is a fun game on Rock 🤜, Paper 🫲 and Scissors ✌️.Let's begin!!! 🏁🏁🏁 Write EITHER Rock, Paper, or Scissors and press 🆗 please! You need to do this 5 times! GOOD LUCK!"
+    ).toLowerCase();
+    if (!["rock", "paper", "scissors"].includes(playerChoice)) {
+      alert("Uups! This is not one of 'rock', 'paper', or 'scissors'🧨");
+    }
+  } while (!["rock", "paper", "scissors"].includes(playerChoice));
+  return playerChoice;
 }
 
+//Ruling the game
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "It's a tie!";
-    } else if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-        return `You win! ${playerSelection} beats ${computerSelection}.`;
-    } else {
-        return `You lost! ${computerSelection} beats ${playerSelection}.`;
-    }
+  if (playerSelection === computerSelection) {
+    return "It's a tie! 🤝";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `You win 📣! ${playerSelection} beats ${computerSelection}.`;
+  } else {
+    return `You lost 📣! ${computerSelection} beats ${playerSelection}. Caamon! You can do it better!!!`;
+  }
 }
 
+//Function for score counting
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+  let playerScore = 0;
+  let computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getPlayerChoice();
-        const computerSelection = computerPlay();
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = computerPlay();
 
-        console.log(`Player chose: ${playerSelection}`);
-        console.log(`Computer chose: ${computerSelection}`);
+    console.log(`Player chose: ${playerSelection}`);
+    console.log(`Computer chose: ${computerSelection}`);
 
-        const roundResult = playRound(playerSelection, computerSelection);
-        console.log(roundResult);
+    const roundResult = playRound(playerSelection, computerSelection);
+    console.log(roundResult);
 
-        if (roundResult.includes("win")) {
-            playerScore++;
-        } else if (roundResult.includes("lost")) {
-            computerScore++;
-        }
-
-        console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`);
+    if (roundResult.includes("win")) {
+      playerScore++;
+    } else if (roundResult.includes("lost")) {
+      computerScore++;
     }
 
-    if (playerScore > computerScore) {
-        console.log("Congratulations! You won the game! 🏆");
-    } else if (playerScore < computerScore) {
-        console.log("Sorry, you lost the game! 😢");
-    } else {
-        console.log("The game ends in a tie!");
-    }
+    console.log(
+      `Player Score : ${playerScore} | Computer Score: ${computerScore}`
+    );
+  }
+
+  if (playerScore > computerScore) {
+    console.log("Congratulations! You won the game! 🏆🥇🎉");
+  } else if (playerScore < computerScore) {
+    console.log("Sorry, you lost the game! 🧨🧨 But it was a nice try!");
+  } else {
+    console.log("The game ends in a tie! 🤝");
+  }
 }
 
 game();
 
-const newGame = prompt("Do you want to play again? (yes or no)").toLowerCase();
+//Start a new game
+const newGame = prompt(
+  "Do you want to play again? (yes or no)🏁🤜🫲✌️"
+).toLowerCase();
 if (newGame === "yes") {
-    location.reload();
+  location.reload();
 } else {
-    console.log("Thank you for playing!");
+  console.log("Thank you for playing!");
 }
